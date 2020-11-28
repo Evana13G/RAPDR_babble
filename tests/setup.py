ef6167fc1279429e8d9286d4fc203ee32fcb2f7b
Keenan Rhea <keenanrhea@gmail.com>
2020-11-27 23:44:33 -0500

    changed test_scripts to tests

--- tests/setup.py
+++ tests/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['test_scripts'],
+     packages=['tests'],
      package_dir={'': 'src'}
 )
 
