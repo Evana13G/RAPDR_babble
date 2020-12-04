a39280d7861ffd8ad615e6c4c85632ec5870d252
Evana <Evana13G@gmail.com>
2020-12-04 13:31:15 -0500

    Made some minor tweaks to the code

--- test/setup.py
+++ test/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['tests'],
+     packages=['test'],
      package_dir={'': 'src'}
 )
 
